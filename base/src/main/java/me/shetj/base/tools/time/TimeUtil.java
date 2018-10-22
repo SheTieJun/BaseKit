6468aa1a8cd0c743b1077171668a9238f17b2255
shetj <375105540@qq.com>
2018-10-22 18:05:12 +0800

    time

--- base/src/main/java/me/shetj/base/tools/time/TimeUtil.java
+++ base/src/main/java/me/shetj/base/tools/time/TimeUtil.java
@@ -46,6 +46,34 @@ public class TimeUtil {
 
 	}
 
+	public static String formatTime(long ms) {
+
+		int ss = 1000;
+		int mi = ss * 60;
+		int hh = mi * 60;
+		int dd = hh * 24;
+
+		long day = ms / dd;
+		long hour = (ms - day * dd) / hh;
+		long minute = (ms - day * dd - hour * hh) / mi;
+		long second = (ms - day * dd - hour * hh - minute * mi) / ss;
+		long milliSecond = ms - day * dd - hour * hh - minute * mi - second * ss;
+
+		String strDay = day < 10 ? "0" + day : "" + day;
+		//天
+		String strHour = hour < 10 ? "0" + hour : "" + hour;
+		//小时
+		String strMinute = minute < 10 ? "0" + minute : "" + minute;
+		//分钟
+		String strSecond = second < 10 ? "0" + second : "" + second;
+		//秒
+		String strMilliSecond = milliSecond < 10 ? "0" + milliSecond : "" + milliSecond;
+		//毫秒
+		strMilliSecond = milliSecond < 100 ? "0" + strMilliSecond : "" + strMilliSecond;
+
+		return strMinute + "：" + strSecond;
+	}
+
 	/**
 	 * 方法名：  formatFromNoformt	<br>
 	 * 方法描述：返回时间差时间<br>
