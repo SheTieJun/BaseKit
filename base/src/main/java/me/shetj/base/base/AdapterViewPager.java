8322201a9d0ff910724b790e6a1db171fe6af5e6
shetj <375105540@qq.com>
2019-03-07 12:41:00 +0800

    viewpager 动态删除

--- base/src/main/java/me/shetj/base/base/AdapterViewPager.java
+++ base/src/main/java/me/shetj/base/base/AdapterViewPager.java
@@ -2,9 +2,9 @@ package me.shetj.base.base;
 
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
-import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
@@ -16,15 +16,18 @@ import java.util.List;
 public class AdapterViewPager extends FragmentStatePagerAdapter {
     private List<BaseFragment> mList;
     private  List<String> mTitles;
+    private  FragmentManager mFragmentManager;
 
     public AdapterViewPager(FragmentManager fragmentManager, List<BaseFragment> list) {
         super(fragmentManager);
+        mFragmentManager = fragmentManager;
         this.mList = list;
     }
 
 
     public AdapterViewPager(FragmentManager fragmentManager, List<BaseFragment> list, List<String> titles) {
         super(fragmentManager);
+        mFragmentManager = fragmentManager;
         this.mList = list;
         this.mTitles = titles;
     }
@@ -52,12 +55,29 @@ public class AdapterViewPager extends FragmentStatePagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        BaseFragment f = (BaseFragment) super.instantiateItem(container, position);
-        View view = f.getView();
-        if (view != null) {
-            container.addView(view);
+
+        Fragment instantiateItem = ((Fragment) super.instantiateItem(container, position));
+        Fragment item = mList.get(position);
+        if (instantiateItem == item) {
+            return instantiateItem;
+        } else {
+            //如果集合中对应下标的fragment和fragmentManager中的对应下标的fragment对象不一致，那么就是新添加的，所以自己add进入；这里为什么不直接调用super方法呢，因为fragment的mIndex搞的鬼，以后有机会再补一补。
+            mFragmentManager.beginTransaction().add(container.getId(), item).commitNowAllowingStateLoss();
+            return item;
         }
-        return f;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        Fragment fragment = (Fragment) object;
+        //如果getItemPosition中的值为PagerAdapter.POSITION_NONE，就执行该方法。
+        if (mList.contains(fragment)) {
+            super.destroyItem(container, position, fragment);
+            return;
+        }
+        //自己执行移除。因为mFragments在删除的时候就把某个fragment对象移除了，所以一般都得自己移除在fragmentManager中的该对象。
+        mFragmentManager.beginTransaction().remove(fragment).commitNowAllowingStateLoss();
+
     }
 
     @Override
