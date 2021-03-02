1b7a7000404475706c934ff74ab59a339bcdcb35
shetj <375105540@qq.com>
2021-03-02 17:13:31 +0800

    [U] 1. 记录一些东西 2. 动态代理实现

--- app/src/main/java/shetj/me/base/test/TestProxy.kt
+++ app/src/main/java/shetj/me/base/test/TestProxy.kt
@@ -1,20 +1,83 @@
 package shetj.me.base.test
 
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.Method
-import java.lang.reflect.Proxy
+import me.shetj.base.ktx.toJson
+import java.lang.reflect.*
+import java.util.*
 
 
-class TestProxy : InvocationHandler {
+class TestProxy<T> : InvocationHandler {
+    private var target //维护一个目标对象
+            : T? = null
+
+    constructor(target: T?) {
+        this.target = target
+    }
+
 
     override fun invoke(proxy: Any, method: Method?, args: Array<out Any>?): Any? {
-        println("测试：${method?.name}")
-        method?.genericReturnType
-        method?.genericParameterTypes  //获取到参数类型
-        method?.annotations //获取到所有注解
-        method?.parameterTypes
+        println("测试：${method?.name.toJson()}\n" +
+                "genericReturnType:${method?.genericReturnType}\n" +
+                "parameterAnnotations：${method?.parameterAnnotations?.size}\n" +
+                "annotations：${method?.annotations?.size}\n" +
+                "parameterTypes：${method?.parameterTypes?.size}\n" +
+                "genericParameterTypes:${method?.genericParameterTypes?.size}\n")
 
-        return method?.invoke(this,args)
+        method?.parameterAnnotations?.forEach {
+
+
+        }
+
+
+        method?.parameterTypes?.forEach {
+            println(getRawType(it)?.simpleName)
+
+
+        }
+
+        method?.genericParameterTypes?.forEach {
+            println(getRawType(it)?.simpleName)
+
+        }
+        target.let {
+            method!!.invoke(target, *(args ?: arrayOfNulls<Any>(0)))
+        }
+        return null
+    }
+
+
+    fun getRawType(type: Type): Class<*>? {
+        Objects.requireNonNull(type, "type == null")
+        if (type is Class<*>) {
+            // Type is a normal class.
+            return type
+        }
+        if (type is ParameterizedType) {
+
+            // I'm not exactly sure why getRawType() returns Type instead of Class. Neal isn't either but
+            // suspects some pathological case related to nested classes exists.
+            val rawType = type.rawType
+            require(rawType is Class<*>)
+            return rawType
+        }
+        if (type is GenericArrayType) {
+            val componentType = type.genericComponentType
+            return java.lang.reflect.Array.newInstance(getRawType(componentType), 0).javaClass
+        }
+        if (type is TypeVariable<*>) {
+            // We could use the variable's bounds, but that won't work if there are multiple. Having a raw
+            // type that's more general than necessary is okay.
+            return Any::class.java
+        }
+        if (type is WildcardType) {
+            return getRawType(type.upperBounds[0])
+        }
+        throw IllegalArgumentException(
+                "Expected a Class, ParameterizedType, or "
+                        + "GenericArrayType, but <"
+                        + type
+                        + "> is of type "
+                        + type.javaClass.name
+        )
     }
 }
 
@@ -24,10 +87,16 @@ class TestProxy : InvocationHandler {
  */
 class ProxyFactory {
     companion object {
-        inline fun <reified T> getProxy(): T {
-            val proxy = TestProxy()
+
+
+        //动态代理本质是静态代理是一样的，本质还是会有具体类进行实现
+
+        inline fun <reified T> getProxy(tag: T?): T {
             val clazz = T::class.java
+            val proxy = TestProxy(tag)
             return Proxy.newProxyInstance(clazz.classLoader, arrayOf(clazz), proxy) as T
         }
     }
 }
+
+
--- base/src/main/java/me/shetj/base/ktx/ClassExt.kt
+++ base/src/main/java/me/shetj/base/ktx/ClassExt.kt
@@ -1,8 +1,8 @@
 package me.shetj.base.ktx
 
 import com.google.gson.internal.`$Gson$Types`
-import java.lang.reflect.ParameterizedType
-import java.lang.reflect.Type
+import java.lang.reflect.*
+import java.util.*
 
 /**
  * A<C>  -> getClazz(this) -> C
@@ -24,4 +24,41 @@ fun <T> getObjByClassArg(obj: Any,position:Int = 0):T{
 fun getParameterizedType(type: Type,typeArguments:Type): ParameterizedType? {
     // Type type = com.google.gson.internal.$Gson$Types.newParameterizedTypeWithOwner(null, ArrayList.class, clazz); = ArrayList<clazz>
     return `$Gson$Types`.newParameterizedTypeWithOwner(null, type, typeArguments)
+}
+
+
+
+fun getRawType(type: Type): Class<*>? {
+    Objects.requireNonNull(type, "type == null")
+    if (type is Class<*>) {
+        // Type is a normal class.
+        return type
+    }
+    if (type is ParameterizedType) {
+
+        // I'm not exactly sure why getRawType() returns Type instead of Class. Neal isn't either but
+        // suspects some pathological case related to nested classes exists.
+        val rawType = type.rawType
+        require(rawType is Class<*>)
+        return rawType
+    }
+    if (type is GenericArrayType) {
+        val componentType = type.genericComponentType
+        return java.lang.reflect.Array.newInstance(getRawType(componentType), 0).javaClass
+    }
+    if (type is TypeVariable<*>) {
+        // We could use the variable's bounds, but that won't work if there are multiple. Having a raw
+        // type that's more general than necessary is okay.
+        return Any::class.java
+    }
+    if (type is WildcardType) {
+        return getRawType(type.upperBounds[0])
+    }
+    throw IllegalArgumentException(
+            "Expected a Class, ParameterizedType, or "
+                    + "GenericArrayType, but <"
+                    + type
+                    + "> is of type "
+                    + type.javaClass.name
+    )
 }
