30ca021115d61ea34d3624ddb572a878384cfd53
shetj <375105540@qq.com>
2022-06-17 17:31:31 +0800

    [update]Calculate execution time

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://jitpack.io" )
+        maven("https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
@@ -12,15 +12,14 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         mavenLocal()
-        maven("https://jitpack.io" )
+        maven("https://jitpack.io")
     }
 }
 rootProject.name = "BaseKit"
-include (":baseKit")
-include (":app")
+include(":baseKit")
+include(":app")
 
 
-/*
 //初始化阶段开始时间
 var beginOfSetting = System.currentTimeMillis()
 //配置阶段开始时间
@@ -28,7 +27,7 @@ var beginOfConfig = 0L
 //配置阶段是否开始了，只执行一次
 var configHasBegin = false
 //存放每个 build.gradle 执行之前的时间
-var beginOfProjectConfig = HashMap<Any,Any>()
+var beginOfProjectConfig = HashMap<Any, Long>()
 //执行阶段开始时间
 var beginOfTaskExecute = 0L
 //初始化阶段执行完毕
@@ -38,18 +37,18 @@ gradle.projectsLoaded {
 
 
 //build.gradle 执行前
-gradle.beforeProject {project ->
-    if(!configHasBegin){
+gradle.beforeProject {
+    if (!configHasBegin) {
         configHasBegin = true
         beginOfConfig = System.currentTimeMillis()
     }
-    beginOfProjectConfig.put(project,System.currentTimeMillis())
+    beginOfProjectConfig.put(project, System.currentTimeMillis())
 }
 
 //build.gradle 执行后
-gradle.afterProject {  project ->
-    val begin = beginOfProjectConfig.get(project)
-    println( "配置阶段，$project 耗时：${System.currentTimeMillis() - begin} ms")
+gradle.afterProject {
+    val begin = beginOfProjectConfig[project] ?:0L
+    println("配置阶段，$project 耗时：${System.currentTimeMillis() - begin} ms")
 }
 
 //配置阶段完毕
@@ -59,17 +58,18 @@ gradle.taskGraph.whenReady {
 }
 
 //执行阶段
-gradle.taskGraph.beforeTask {  task ->
-    task.doFirst {
-        task.ext.beginOfTask = System.currentTimeMillis()
+gradle.taskGraph.beforeTask {
+    var startTs = 0L
+    doFirst {
+        startTs = System.currentTimeMillis()
     }
 
-    task.doLast {
-        println( "执行阶段，$task 耗时：${System.currentTimeMillis() - task.ext.beginOfTask} ms")
+    doLast {
+        println("执行阶段，$this 耗时：${System.currentTimeMillis() - startTs} ms")
     }
 }
 
 //执行阶段完毕
 gradle.buildFinished {
     println("执行阶段总耗时：${System.currentTimeMillis() - beginOfTaskExecute}")
-}*/
+}
