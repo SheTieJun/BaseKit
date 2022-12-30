ef88609c09fece1ea1610dec193d682d17365d96
SheTieJun <375105540@qq.com>
2022-12-30 14:03:16 +0800

    Fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,12 +37,12 @@ android {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     //产品变种组,
