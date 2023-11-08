ad4ec17137671d63b2ac0342ccae54ead7fa12ad
SheTieJun <375105540@qq.com>
2023-11-08 11:58:13 +0800

    :bug:  fix 1.9.0 build room error

--- buildSrc/src/main/kotlin/tools/DependencyLibs.kt
+++ buildSrc/src/main/kotlin/tools/DependencyLibs.kt
@@ -216,7 +216,7 @@ object DependencyLibs {
         }
 
         object Room {
-            private const val version = "2.5.2"
+            private const val version = "2.6.0"  //kotlin 1.9.0之后需要升级到2.6.0
             const val runtime = "androidx.room:room-runtime:$version"
             const val ktx = "androidx.room:room-ktx:$version"
             const val compiler = "androidx.room:room-compiler:$version"
