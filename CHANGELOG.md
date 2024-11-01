f5c861e46c1fe1766cf019754f658fcbeedcbd7b
SheTieJun <375105540@qq.com>
2024-11-01 18:09:42 +0800

    ✨ 添加CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Changelog
+
+My awesome project that provides a lot of useful features, like:
+
+- Feature 1
+- Feature 2
+- and Feature 3
+
+## [Unreleased]
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.changelog.ChangelogSectionUrlBuilder
+import org.jetbrains.changelog.date
+import tools.versionName
+
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -21,6 +25,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.23.3"
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.changelog)
 }
 
 //apply(from = "https://gist.githubusercontent.com/SheTieJun/f4cb1bd33997c2b46d9e3df40b95a02e/raw/c4b826d3ca4415071097b1642c9b80e50f3f1ad0/subprojects-maven-publishing.gradle")
@@ -28,3 +33,27 @@ plugins {
 apply(from = "gradle/detekt.gradle")
 
 tasks.register<tools.PrintProjectStructureTask>("printProjectStructure")
+
+
+changelog {
+    version.set(project.versionName)
+    path.set(file("CHANGELOG.md").canonicalPath)
+    header.set(provider { "[${version.get()}] - ${date()}" })
+    headerParserRegex.set("""(\d+\.\d+)""".toRegex())
+    introduction.set(
+        """
+        My awesome project that provides a lot of useful features, like:
+        
+        - Feature 1
+        - Feature 2
+        - and Feature 3
+        """.trimIndent()
+    )
+    itemPrefix.set("-")
+    keepUnreleasedSection.set(true)
+    unreleasedTerm.set("[Unreleased]")
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+    lineSeparator.set("\n")
+    combinePreReleases.set(true)
+    repositoryUrl = "https://github.com/SheTieJun/BaseKit"
+}
